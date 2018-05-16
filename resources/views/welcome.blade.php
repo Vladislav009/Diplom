1cf3ec70e445218e630541f6d613ef83d0a771c8
LAPTOP-GM8QIO64\kulak <kulakvlad@gmail.com>
2018-05-16 16:52:46 +0500

    Вывод категорий на страницу

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,6 @@
         background-color: #fff;
         color: #636b6f;
         font-family: 'Raleway', sans-serif;
-        font-weight: 100;
         height: 100vh;
         margin: 0;
     }
@@ -52,7 +51,7 @@
     }
 
     .links > a {
-        color: #636b6f;
+        /* color: #636b6f; */
         padding: 0 25px;
         font-size: 12px;
         font-weight: 600;
@@ -79,29 +78,52 @@
         </div>
         @endif
 
+        @foreach($categories as $category)
+
         <div class="container">
-         <div class="panel-body">
-            <form action="@if(empty($entity)){{ route('questions.store') }}@else{{ route('questions.update', $entity->id) }}@endif" method="post">
+            <div class="accordion" id="accordionExample">
+              <div class="card">
+                <div class="card-header" id="headingOne">
+                  <h5 class="mb-0">
+                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#cat1" aria-expanded="true" aria-controls="cat1">
+                      {{$category->title}}
+                  </button>
+              </h5>
+          </div>
 
-             {{ csrf_field() }}
-             @isset($entity)
-             {{ method_field('PUT') }}
-             @endisset
-             <div class="row">
-                 @include('admin.fields.text', ['field' => 'title', 'name' => 'Title'])
+          <div id="cat1" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+              <div class="card-body">
+                <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</p>
+            </div>
+        </div>
+    </div>
+</div>
 
-                 @include('admin.fields.textarea', ['field' => 'body', 'name' => 'Body', 'rows' => 10])
-                 @include('admin.fields.select', ['field' => 'category_id', 'name' => 'Category', 'options' => $categories])
-             </div>
-             <input class="btn btn-primary btn-lg" type="submit" value="save"> 
+@endforeach
 
-         </form>
-     </div>
+<h4>Задать свой вопрос</h4>
 
 
+<div class="panel-body">
+    <form action="@if(empty($entity)){{ route('questions.store') }}@else{{ route('questions.update', $entity->id) }}@endif" method="post">
 
- </div>
+       {{ csrf_field() }}
+       @isset($entity)
+       {{ method_field('PUT') }}
+       @endisset
+       <div class="row">
+           @include('admin.fields.text', ['field' => 'title', 'name' => 'Title'])
+           @include('admin.fields.select', ['field' => 'category_id', 'name' => 'Category', 'options' => $categories])
+       </div>
+       <input class="btn btn-primary btn-lg" type="submit" value="save"> 
 
- <script src="{{ asset('js/app.js') }}"></script>
+   </form>
+</div>
+
+
+
+</div>
+
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
